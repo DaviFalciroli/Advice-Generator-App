fce5abad06a0c0f7a37d5b0343bad4c2638366ad
Davi Falciroli <falciroli3020@gmail.com>
2025-01-15 16:49:59 -0300

    foi divido em dois componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,31 @@
-import { useEffect, useState } from 'react'
-import diceImg from './images/icon-dice.svg'
-import patternDesktop from './images/pattern-divider-desktop.svg'
-import patternMobile from './images/pattern-divider-mobile.svg'
-import { AdviceService } from './service/advice'
-
+import { useEffect, useState } from 'react';
+import { AdviceService } from './service/advice';
+import Card from './components/Card';
+import AdviceCard from './components/AdviceCard';
 
 const App = () => {
+    const [advice, setAdvice] = useState<{ id: number; advice: string } | null>(null);
 
-    const [advice, setAdvice] = useState()
+    const fetchData = async () => {
+        const data = await AdviceService.getAdvice();
+        setAdvice(data.slip);
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            const data = await AdviceService.getAdvice();
-
-            setAdvice(data)
-        }
-
-        fetchData()
-
-    }, [])
+        fetchData();
+    }, []);
 
     return (
-        <div className="bg-darkGrayishBlue h-screen flex justify-center items-center">
-            <div className="bg-grayishBlue flex flex-col items-center rounded-lg max-w-[350px] m-5">
-                <h1 className="text-neonGreen uppercase tracking-[5px] text-lg mt-7">Advice</h1>
-
-                <p className='p-6 text-center text-2xl'>DFSHYSAU</p>
-
-                <picture>
-                    <source srcSet={patternMobile} media='(max-width: 768px)' />
-                    <img src={patternDesktop} alt="Pattern Desktop" />
-                </picture>
-
-                <button className='bg-neonGreen p-3 rounded-full h-14 w-14 translate-y-1/2 hover:cursor-pointer hover:shadow-md hover:shadow-neonGreen'>
-                    <img src={diceImg} alt="Dado" className='w-full ' />
-                </button>
-
-            </div>
+        <div className="bg-darkBlue h-screen flex justify-center items-center">
+            <Card>
+                <AdviceCard
+                    adviceId={advice?.id}
+                    adviceText={advice?.advice}
+                    onFetchAdvice={fetchData}
+                />
+            </Card>
         </div>
-    )
-}
+    );
+};
 
-export default App
+export default App;
